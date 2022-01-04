7eac52f3298c4ede1a9936c2a1b48720e761891c
Abir Pal <abirpal282002@gmail.com>
2022-01-04 11:56:36 +0530

    Making changes on the navbar section

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,10 +5,26 @@ const jwt = require('jsonwebtoken');
 const { SECRET_KEY } = process.env
 
 exports.homePage = async (req, res) => {
-    console.log(req.cookies);
+    let flag;
+    let userProfile = "";
+    console.log(req.cookies.Token);
+    if(!(req.cookies.Token)){
+        // If no token Render navbar 1
+        flag = "One"
+        
+    }else{
+        // If Token is present render navbar 2-(Profile type)
+        flag = "Two"
+        const decode = jwt.verify(req.cookies.Token, SECRET_KEY);
+        console.log(decode.email);
+        userProfile = await User.findOne({email : decode.email})
+        userProfile =userProfile.username
+    }
+    console.log(flag);
+
     const articles = await Articles.find({})
-    const user = await User.find({})
-    res.render('index', {articles : articles, user : user})
+    const users = await User.find({})
+    res.render('index', {articles : articles, users : users, flag : flag, user : userProfile})
     // res.send("Hi")
 }
 
